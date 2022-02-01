c715d9349a78f4e6c644a1e4c1413115f6793c92
Serrano Paul <paul.serrano08374@gmail.com>
2022-02-01 18:44:11 +0100

    chess pieces, table

---
+++ index.php
@@ -0,0 +1,99 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <main>
+    <div class="plateau">
+
+    </div>
+    <table>
+<tr>
+	<td class="bordure" draggable="true"><img src="./img/noire-tour.png" /></td>
+	<td class="bordure case-noire" draggable="true"><img src="./img/noir-cheval.png"/></td>
+	<td class="bordure" draggable="true"><img src="./img/noir-fou.png"></td>
+	<td class="bordure" draggable="true"><img src="./img/noire-dame.png"/></td>
+	<td class="bordure" draggable="true"><img src="./img/noir-roi.png"></td>
+	<td class="bordure case-noire" draggable="true"><img src="./img/noir-fou.png"/></td>
+	<td class="bordure" draggable="true"><img src="./img/noir-cheval.png"></td>
+	<td class="bordure case-noire" draggable="true"><img src="./img/noire-tour.png"/></td>
+	</tr>
+<tr>
+	<td class="bordure case-noire" draggable="true"><img src="./img/noir-pion.png"/></td>
+	<td class="bordure" draggable="true"><img src="./img/noir-pion.png"></td>
+	<td class="bordure case-noire" draggable="true"><img src="./img/noir-pion.png"/></td>
+	<td class="bordure" draggable="true"><img src="./img/noir-pion.png"></td>
+	<td class="bordure case-noire" draggable="true"><img src="./img/noir-pion.png"/></td>
+	<td class="bordure" draggable="true"><img src="./img/noir-pion.png"></td>
+	<td class="bordure case-noire" draggable="true"><img src="./img/noir-pion.png"/></td>
+	<td class="bordure" draggable="true"><img src="./img/noir-pion.png"> </td>
+</tr>
+
+<tr>
+	<td class="bordure" draggable="true"><p>blank</p></td>
+	<td class="bordure case-noire" draggable="true"><p>blank</p></td>
+	<td class="bordure" draggable="true"><p>blank</p></td>
+	<td class="bordure case-noire" draggable="true"><p>blank</p></td>
+	<td class="bordure" draggable="true"><p>blank</p></td>
+	<td class="bordure case-noire" draggable="true"><p>blank</p></td>
+	<td class="bordure" draggable="true"><p>blank</p></td>
+	<td class="bordure case-noire" draggable="true"><p>blank</p></td>
+</tr>
+<tr>
+    <td class="bordure" draggable="true"><p>blank</p></td>
+	<td class="bordure case-noire" draggable="true"><p>blank</p></td>
+	<td class="bordure" draggable="true"><p>blank</p></td>
+	<td class="bordure case-noire" draggable="true"><p>blank</p></td>
+	<td class="bordure" draggable="true"><p>blank</p></td>
+	<td class="bordure case-noire" draggable="true"><p>blank</p></td>
+	<td class="bordure" draggable="true"><p>blank</p></td>
+	<td class="bordure case-noire" draggable="true"><p>blank</p></td>
+</tr>
+<tr>
+    <td class="bordure" draggable="true"><p>blank</p></td>
+	<td class="bordure case-noire" draggable="true"><p>blank</p></td>
+	<td class="bordure" draggable="true"><p>blank</p></td>
+	<td class="bordure case-noire" draggable="true"><p>blank</p></td>
+	<td class="bordure" draggable="true"><p>blank</p></td>
+	<td class="bordure case-noire" draggable="true"><p>blank</p></td>
+	<td class="bordure" draggable="true"><p>blank</p></td>
+	<td class="bordure case-noire" draggable="true"><p>blank</p></td>
+</tr>
+<tr>
+    <td class="bordure" draggable="true"><p>blank</p></td>
+	<td class="bordure case-noire" draggable="true"><p>blank</p></td>
+	<td class="bordure" draggable="true"><p>blank</p></td>
+	<td class="bordure case-noire" draggable="true"><p>blank</p></td>
+	<td class="bordure" draggable="true"><p>blank</p></td>
+	<td class="bordure case-noire" draggable="true"><p>blank</p></td>
+	<td class="bordure" draggable="true"><p>blank</p></td>
+	<td class="bordure case-noire" draggable="true"><p>blank</p></td>
+</tr>
+<tr>
+	<td class="bordure" draggable="true"><img src="./img/blanc-pion.png"/></td>
+	<td class="bordure case-noire" draggable="true"><img src="./img/blanc-pion.png"/></td>
+	<td class="bordure" draggable="true"><img src="./img/blanc-pion.png"/></td>
+	<td class="bordure case-noire" draggable="true"><img src="./img/blanc-pion.png"/></td>
+	<td class="bordure" draggable="true"><img src="./img/blanc-pion.png"/></td>
+	<td class="bordure case-noire" draggable="true"><img src="./img/blanc-pion.png"/></td>
+	<td class="bordure" draggable="true"><img src="./img/blanc-pion.png"/></td>
+	<td class="bordure case-noire" draggable="true"><img src="./img/blanc-pion.png"/></td>
+</tr>
+<tr>
+	<td class="bordure case-noire" draggable="true"><img src="./img/blanc-tour.png"/></td>
+	<td class="bordure" draggable="true"><img src="./img/blanc-cheval.png"/></td>
+	<td class="bordure case-noire" draggable="true"><img src="./img/blanc-fou.png"/></td>
+	<td class="bordure" draggable="true"><img src="./img/blanc-dame.png"/></td>
+	<td class="bordure case-noire" draggable="true"><img src="./img/blanc-roi.png"/></td>
+	<td class="bordure" draggable="true"><img src="./img/blanc-fou.png"/></td>
+	<td class="bordure case-noire" draggable="true"><img src="./img/blanc-cheval.png"/></td>
+	<td class="bordure" draggable="true"><img src="./img/blanc-tour.png"/></td>
+</tr>
+</table>
+    </main>
+</body>
+</html>
